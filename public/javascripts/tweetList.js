0c13a2d355c489dfe09f3b78455a6fa6beb10eaf
Christopher Gillis <Christopher.L.Gillis@gmail.com>
2013-11-16 08:12:07 -0500

    showing tweets added preview

--- public/javascripts/tweetList.js
+++ public/javascripts/tweetList.js
@@ -1,37 +1,22 @@
 $(function() {
     tweetList = function tweetList() {
-      //  var _ = require('lodash');
-
         var tweets = [];
 
         var self = this; // annoying, but done for scoping
 
         self.add = function add(newTweet) {
-            var tweet = tweetCheck(newTweet);
-            tweets = [tweet].concat(tweets);
+            if(!newTweet) {
+                return;
+            }
 
-            self.trigger("add", tweet);
+            tweets = [newTweet].concat(tweets);
+
+            self.trigger("add", newTweet);
             return self;
         };
 
         self.getTweets = function getTweets() {
             return tweets;
-        };
-
-        var requiredFields = [
-            'from',
-            'to',
-            'message'
-        ];
-
-        var tweetCheck = function(tweet) {
-            _.forEach(requiredFields, function(f) {
-                if(!tweet[f]) {
-                    throw Error("Missing field: " + f);
-                }
-            });
-
-            return tweet;
         };
 
         $.observable(self);
--- public/javascripts/tweetListPresenter.js
+++ public/javascripts/tweetListPresenter.js
@@ -2,21 +2,45 @@
  * Created by minitosh on 11/8/13.
  */
 $(function() {
-    var myTweetList = new tweetList();
+    var myTweetList = new tweetList(),
+        tweetPreview = $("#tweetPreview"),
+        toHandle = $("#toHandle"),
+        fromHandle = $("#fromHandle"),
+        messageHandle = $("#messageHandle");
+
+
 
     $('form').on('submit', function(e) {
         e.preventDefault();
         var tweet = getTweet();
+        var errors = validate(tweet);
+        if(errors.hasErrors()) {
+            console.log('The following errors occurred');
+            console.log(errors);
+            return false;
+        }
+
+        $("form").find("input[type=text]").val("");
         myTweetList.add(tweet);
 
         return false;
     });
 
+    toHandle.keyup(updatePreview);
+    fromHandle.keyup(updatePreview);
+    messageHandle.keyup(updatePreview);
+
+    function updatePreview() {
+        var toVal = toHandle.val() || '_______';
+        var fromVal = fromHandle.val() || '_______';
+        var messageVal = messageHandle.val() || '_______';
+        tweetPreview.html("Love is in the air! " + fromVal + " loves " + toVal + " more than " + messageVal + "! #lymt");
+    }
+
     function getTweet() {
-        var to = $("#toHandle").val();
-        var from = $("#fromHandle").val();
-        var message = $("#message").val();
-        $("form").find("input[type=text]").val("");
+        var to = toHandle.val();
+        var from = fromHandle.val();
+        var message = messageHandle.val();
         return {
             to: to,
             from: from,
@@ -24,8 +48,29 @@ $(function() {
         };
     }
 
+    var requiredFields = [
+        'from',
+        'to',
+        'message'
+    ];
+
+    function validate(tweet) {
+        var error = {};
+        _.forEach(requiredFields, function(f) {
+            error[f] = !!tweet[f] ? undefined : 'Missing ' + f;
+        });
+
+        error.hasErrors = function() {
+            return !!(error.to || error.from || error.message);
+        }
+
+        return error;
+    }
+
     myTweetList.on("add", function(tweet) {
         $.post('/tweet', tweet);
     });
+
+    updatePreview();
 
 });
